06b69ce0a920b2d2c444c356386829cdb924db65
CarloDettori <dettoric07@gmail.com>
2024-11-07 19:31:24 +0100

    adding alternation script at button clicking: backgrownd color (white-darkGrey), button inner text(accendi-spegni), bulb img change berween (whiteBulb-yellowBulb)

--- README.md
+++ README.md
@@ -13,5 +13,6 @@ Facciamo accendi e spegni:
 - Al primo click la lampadina si accende e nel bottone compare la scritta "Spegni"
 - Al secondo click la lampadina si spegne e nel bottone compare la scritta "Accendi"
 - E così via...
-Per fare questo bonus potremmo aver bisogno di del metodo string.includes() :occhiolino:
+Per fare questo bonus potremmo aver bisogno del metodo string.includes() 
+
 Buon Lavoro e buon divertimento!
--- script/script.js
+++ script/script.js
@@ -1,2 +1,19 @@
 "use strict"
-console.clean()
+console.clear()
+
+const button = document.getElementById("button");
+const bulb = document.getElementById("img");
+const body = document.getElementById("body")
+
+button.addEventListener("click", function () {
+    if (button.innerText.includes("ACCENDI")) {
+        button.innerText = "SPEGNI";
+        bulb.src = "img/yellow_lamp.png";
+        bulb.style.transform = "scaleX(-1)";
+    } else {
+        button.innerText = "ACCENDI";
+        bulb.src = "img/white_lamp.png";
+        bulb.style.transform = "scaleX(1)";
+    }
+    body.classList.toggle("off-bg");
+})
